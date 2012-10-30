098a8250cb39e1f66372fd7e08ecc8c3ac795baf
jack.zh <gitcafe.zh@gmail.com>
2012-10-30 12:00:22 +0800

    point add

--- weakpoint/bootstrap.py
+++ weakpoint/bootstrap.py
@@ -238,7 +238,7 @@ content += '''
 # ====================ribbon====================
 if ribbon_enable:
     content += '''
-    <a href="https://github.com/onesuper/weakpoint"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_white_ffffff.png" alt="Fork me on GitHub"></a>'''
+    <a href="https://github.com/zhblog"><img style="position: absolute; top: 0; right: 0; border: 0;" src="/blog/img/f.png" alt="Fork me on GitHub"></a>'''
 
     
 # ==================html ends=====================
